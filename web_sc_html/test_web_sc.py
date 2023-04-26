f4ee0360662bb3b11d535539ff9c8dd80b6c6f62
Jesús Chaves Acero <84520072+jchaves1406@users.noreply.github.com>
2023-04-26 14:00:15 -0500

    Update test_web_sc.py

--- web_sc_html/test_web_sc.py
+++ web_sc_html/test_web_sc.py
@@ -1,12 +1,11 @@
 from bs4 import BeautifulSoup
-from web_sc_html import extraer_atributos_casa, \
-    obtener_bloques_informacion
+from web_sc_html import descargar_pagina
 from unittest import mock
 
 
 @mock.patch('web_sc_functions.descargar_pagina')
 def test_descargar_pagina(mock_descargar_pagina):
     url = "https://www.google.com"
-    page = mock_descargar_pagina(url)
+    page = descargar_pagina(url)
     mock_descargar_pagina.assert_called_once_with(url)
     assert page is not None
