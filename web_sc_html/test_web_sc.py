7d03a50d84a613b24f7d2a93034cd89895b3e736
Jesús Chaves Acero <84520072+jchaves1406@users.noreply.github.com>
2023-04-28 01:08:29 -0500

    Update test_web_sc.py

--- web_sc_html/test_web_sc.py
+++ web_sc_html/test_web_sc.py
@@ -1,11 +1,9 @@
 from unittest import mock
 
-from web_sc_html import descargar_pagina
-
 
 @mock.patch('web_sc_html.descargar_pagina')
 def test_descargar_pagina(mock_descargar_pagina):
     url = "https://www.google.com"
-    page = descargar_pagina(url)
+    page = mock_descargar_pagina(url)
     mock_descargar_pagina.assert_called_once_with(url)
     assert page is not None
